37bd3cafd8c9188fc12a81d0b641422f863c31f7
Dalejan <david1804997@gmail.com>
2018-08-14 14:58:52 -0500

    Se añade la funcionalidad de enviar correos por medio de un servidor en node (ver nodemailer-express-server)

--- scripts/sendMail.js
+++ scripts/sendMail.js
@@ -1,37 +1,46 @@
-var strAllMyEmails = "";
+
+var arrAllMyEmails = { "myMails": [{}] }; //Almacena información volátil de todos los correos
+
+var xhr = new XMLHttpRequest();
+var objMail = {}; //Objeto del email
+var blIsValidated = false; //Checkea si el formulario es válido
+var nuErr = 0; //Lleva la cuenta de errores en el formulario
 
 function sendMail() {
-    const elForm = document.getElementById("email-form");
-    var strJsonForm = "{";
-    var blIsValidated = false;
-    var nuErr = 0;
+    const elForm = document.getElementById("email-form"); //El formulario
 
     for (let i = 0; i < elForm.length; i++) {
-        strJsonForm += "\"" + elForm[i].id + "\"" + ":" + "\"" + elForm[i].value + "\", ";
-        elForm[i].value == "" ? nuErr ++ : nuErr = nuErr;
+        elForm[i].value == "" ? nuErr++ : nuErr = nuErr;
+    }
+
+    objMail = {
+        "name": elForm[0].value,
+        "mail": elForm[1].value,
+        "country": elForm[2].value,
+        "text": elForm[3].value,
     }
 
     nuErr == 0 ? blIsValidated = true : blIsValidated = false;
-
-    if (!blIsValidated) {
-        alertError("Todos los campos son obligatorios")
-    }
-    else {
-        strJsonForm += "}";
-        concatEmails(strJsonForm);
-    }
-    closeModal();
+    !blIsValidated ? alertError("Todos los campos son obligatorios") : concatEmails(objMail);
 }
 
 function alertError(strError) {
     console.log("Error", strError);
 }
 
-function concatEmails(strEmail) {
-    strAllMyEmails += strEmail;
-    console.log("Todos mis correos enviados", strAllMyEmails)
+function concatEmails(objMail) {
+    arrAllMyEmails.myMails.push(objMail); //concatena los correos 
+
+    //Envía correo
+    xhr.open("POST", 'http://localhost:8000/api/email', true);
+    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+    xhr.send("name=" + "" + objMail.name + "" + "&" + "mail=" + "" + objMail.mail + "" + "&" + "country=" + "" + objMail.country + "" + "&" + "text=" + "" + objMail.text + "");
+
+    console.log("Todos mis correos enviados", arrAllMyEmails);
+    closeModal();
 }
 
 function closeModal() {
     window.location.href = "#";
 }
+
